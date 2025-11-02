42139319c83a889b4399fe32598860189de32fbb
IEVN1003-22002257 <jonymendez40@gmail.com>
2025-11-02 17:48:26 -0600

    Practicas

--- src/app/formularios/axb/axb.component.ts
+++ src/app/formularios/axb/axb.component.ts
@@ -1,33 +1,38 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Axb } from './axb';
 
 @Component({
-  selector: 'app-axb',
-  standalone: true,
-  imports: [ReactiveFormsModule], 
-  templateUrl: './axb.component.html',
+  selector: 'app-axb',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule], 
+  templateUrl: './axb.component.html',
 })
 export class AxbComponent {
-  public axbForm: FormGroup;
+  public axbForm: FormGroup;
+  public resultado: number = 0;
+  public calculoRealizado: boolean = false;
+  private axbLogic = new Axb();
 
-  public resultado: number = 0;
-  public calculoRealizado: boolean = false;
+  constructor(private fb: FormBuilder) {
+    this.axbForm = this.fb.group({
+      numeroA: [0, [Validators.required]],
+      numeroB: [0, [Validators.required]]
+    });
+  }
 
-  private axbLogic = new Axb();
+  public realizarCalculo(): void {
+    if (this.axbForm.valid) {
+      
+    
+      const formValues = this.axbForm.value;
 
-  constructor(private fb: FormBuilder) {
-    this.axbForm = this.fb.group({
-      numeroA: [0, [Validators.required, Validators.pattern("^[0-9]*$")]],
-      numeroB: [0, [Validators.required, Validators.pattern("^[0-9]*$")]]
-    });
-  }
+      const valA = Number(formValues.numeroA);
+      const valB = Number(formValues.numeroB);
 
-  public realizarCalculo(): void {
-    if (this.axbForm.valid) {
-      const { numeroA, numeroB } = this.axbForm.value;
-      this.resultado = this.axbLogic.calcular(numeroA, numeroB);
-      this.calculoRealizado = true;
-    }
-  }
+      this.resultado = this.axbLogic.calcular(valA, valB);
+      this.calculoRealizado = true;
+    }
+  }
 }
